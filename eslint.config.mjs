699a8a6741d3d78107c0bff90fe7e216390444a7
Itsuki Shirahige <shiron4710@gmail.com>
2026-03-13 18:50:26 +0900

    Fix eslint hanging by narrowing lint scope

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,14 @@ import shiron from "@shiron-dev/eslint-config";
 
 export default shiron(
   {
+    ignores: [
+      "**/node_modules/**",
+      "**/.wxt/**",
+      "**/build/**",
+      "**/.react-router/**",
+    ],
+  },
+  {
     rules: {
       "unicorn/error-message": "off",
     },
